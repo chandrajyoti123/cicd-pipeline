c2930beaed1304afb3dffef610af76f6c842d780
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2025-01-03 20:07:50 +0530

    next commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useCallback } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
